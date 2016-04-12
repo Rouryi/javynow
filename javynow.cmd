aba3fd41d12014b82ec4b258eed0ce773d17428b
Rouryi <tamuratarou3@gmail.com>
2016-04-12 17:41:22 +0900

    Added files via upload

---
+++ javynow.cmd
@@ -0,0 +1,11 @@
+@if (0)==(0) echo off
+for /f "tokens=1,2 delims=," %%a in ('cscript.exe //nologo //e:jscript "%~f0"^<javy.txt') do (
+ffmpeg.exe -headers "User-Agent: Mozilla/5.0 (Linux; Android 5.1.1; HUAWEI KII-L22 Build/HUAWEIKII-L22) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.105 Mobile Safari/537.36" -i "%%a.m3u8" -c copy -bsf:a aac_adtstoasc "%%b.mp4"
+)
+goto :eof
+@end
+var StdIn=WScript.StdIn;
+while (!StdIn.AtEndOfLine){
+var s=StdIn.ReadLine();
+if(!StdIn.AtEndOfLine) WScript.Echo(s+","+StdIn.ReadLine());
+}
