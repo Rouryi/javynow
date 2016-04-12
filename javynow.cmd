9791306ff3a14dba2bb1131b058003b69cda2d97
Rouryi <tamuratarou3@gmail.com>
2016-04-12 18:04:02 +0900

    add pushd

--- javynow.cmd
+++ javynow.cmd
@@ -1,4 +1,7 @@
 @if (0)==(0) echo off
+pushd %UserProfile%\javynow
+cls
+
 for /f "tokens=1,2 delims=," %%a in ('cscript.exe //nologo //e:jscript "%~f0"^<javy.txt') do (
 ffmpeg.exe -headers "User-Agent: Mozilla/5.0 (Linux; Android 5.1.1; HUAWEI KII-L22 Build/HUAWEIKII-L22) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.105 Mobile Safari/537.36" -i "%%a.m3u8" -c copy -bsf:a aac_adtstoasc "%%b.mp4"
 )
@@ -9,3 +12,6 @@ while (!StdIn.AtEndOfLine){
 var s=StdIn.ReadLine();
 if(!StdIn.AtEndOfLine) WScript.Echo(s+","+StdIn.ReadLine());
 }
+
+popd
+pause
